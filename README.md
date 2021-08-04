2f34d0e7fe438e291dd511be08347784cd13cdbe
Sreejan <sreejan147@gmail.com>
2021-08-04 11:51:59 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# crud-api
+
+A simple crud api built using node.js(express.js) and mongodb to create, read, update and delete blogs.
---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+*/
+require("dotenv").config();
+
+console.log("hello world");
