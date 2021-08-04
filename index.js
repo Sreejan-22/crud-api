45bc18668e63ba0b9f400680d642fe6974a0e9da
Sreejan <sreejan147@gmail.com>
2021-08-04 17:19:48 +0530

    setup connection with mongodb

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const mongoose = require("mongoose");
 /*
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -5,4 +7,22 @@ if (process.env.NODE_ENV !== 'production') {
 */
 require("dotenv").config();
 
-console.log("hello world");
+const app = express(); // Creates an Express application
+const port = 3000;
+
+// connect to MongoDB
+
+const dbURI = `mongodb+srv://blog_admin:${process.env.MONGODB_ACCESS_PASSWORD}@crudapi.jnual.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) =>
+    // listen for requests only after connection to the db has been made
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    })
+  )
+  .catch((err) => console.log(err));
+
+// app.listen(port, () => {
+//   console.log(`App listening on port ${port}`);
+// });
